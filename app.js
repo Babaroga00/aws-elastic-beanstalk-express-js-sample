b7711d392d5d78a4350f320ebaa5685384e81ad5
Babaroga00 <sarevok@blic.net>
2021-03-08 12:54:32 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('babaroga!'));
+app.get('/', (req, res) => res.send('Ma bravo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
